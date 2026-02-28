509c65c7ef4bc79fabbeabb7c5e2a3d7bbba3029
SaMFernandez <fsandroaquiles@gmail.com>
2026-02-28 14:33:33 -0500

    controller actualizado

--- product-service/src/main/java/org/stockify/controller/ProductController.java
+++ product-service/src/main/java/org/stockify/controller/ProductController.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.stockify.Entity.Product;
+import org.stockify.dto.ProductRequest;
 import org.stockify.dto.ProductResponse;
 import org.stockify.service.ProductService;
 
@@ -24,6 +24,23 @@ public class ProductController {
         return ResponseEntity.ok(productService.findAll());
     }
 
+   @GetMapping("/{id}")
+   ResponseEntity <ProductResponse> findById(@PathVariable Integer id){
+        return ResponseEntity.ok(productService.findById(id));
+    }
 
+    @PostMapping("/{guardar}")
+    ResponseEntity <ProductResponse> save(@RequestBody ProductRequest request){
+        return ResponseEntity.ok(productService.save(request));
+    }
 
+    @PutMapping("/{id}")
+    ResponseEntity <ProductResponse> save(@PathVariable Integer id ,@RequestBody ProductRequest request){
+        return ResponseEntity.ok(productService.update( id, request));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> delete(@PathVariable Integer id){
+       productService.findById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
